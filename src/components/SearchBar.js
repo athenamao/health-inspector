dd03061ef7b5e657558419a6b1bae9fe962a1417
Athena Mao <amao1@uci.edu>
2017-10-09 20:03:31 -0700

    made a beautiful search component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            search: ''
+        }
+    }
+
+    handleSearchChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className="searchbar-container">
+                <form className="search_form">
+                    <input
+                        type="search"
+                        name="search"
+                        value={this.state.search}
+                        className="search-input"
+                        placeholder="Search Restaurants"
+                        onChange={this.handleSearchChange}
+                    />
+
+                </form>
+
+            </div>
+
+            
+
+        )
+    }
+}
+
+export default SearchBar
